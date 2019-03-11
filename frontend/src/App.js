7aedd2d82d6decc8d19980a09ace1c15ab236300
Shin Ji Low <lowshinji@gmail.com>
2019-03-11 11:25:14 -0400

    Initial directory page setup with materialize.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Navbar, NavItem, Footer, Card } from 'react-materialize';
+import './App.css'; // Tell Webpack that Button.js uses these styles
+import persons from './database'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>  
-          </p>DYLAN IS THE BEST!</p>
-        
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <UnicornNavbar />
+        <div className="content">
+          <header>
+            <h1>Unicorn Directory</h1>
+          </header>
+
+          {
+            persons.map((person) => (
+              <Card horizontal title={`${person.firstName} ${person.lastName}`}>
+                <p>{person.position}</p>
+                <p>{`Currently at ${person.location}`}</p>
+              </Card>
+            ))
+          }
+        </div>     
+        <UnicornFooter/>
       </div>
     );
   }
 }
+
+const UnicornNavbar = () => (
+  <Navbar>
+    <NavItem onClick={() => console.log('test click')}>About</NavItem>
+    <NavItem href='components.html'>Connect</NavItem>
+    <NavItem href='components.html'>Directory</NavItem>
+    <NavItem href='components.html'>Help</NavItem>
+  </Navbar>
+);
+
+const UnicornFooter = () => (
+  <Footer 
+    moreLinks={
+      <a className="grey-text text-lighten-4 right" href="#!">Give Us Feedback!</a>
+    }
+    links={
+      <ul>
+        <li><a className="grey-text text-lighten-3" href="#!">About</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Connect</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Directory</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Help</a></li>
+      </ul>
+    }
+    className="footer"
+  >
+    <h5 className="white-text">ISAK Unicorn</h5>
+    <p className="grey-text text-lighten-4">At the ISAK Unicorn we strive to bring the students, faculty and alumni all closer together.</p>
+  </Footer>
+);
 
 export default App;
---
+++ frontend/src/database.js
@@ -0,0 +1,44 @@
+const persons = [
+    {
+        firstName: "Naoki",
+        lastName: "Okada",
+        position: "Alumni",
+        college: "Tufts University",
+        location: "Boston, MA, USA",
+        description: "Currently studying Computer Science"
+    },
+    {
+        firstName: "Dylan",
+        lastName: "Oshima",
+        position: "Alumni",
+        college: "Edinburgh University",
+        location: "Edinburgh, Scotland",
+        description: "Currently studying Computer Science"
+    },
+    {
+        firstName: "Thomas",
+        lastName: "Wang",
+        position: "Alumni",
+        college: "Vanderbilt University",
+        location: "Nashville, TN, USA",
+        description: "Currently studying Computer Science"
+    },
+    {
+        firstName: "Quynh Trang",
+        lastName: "Ho Thi",
+        position: "Alumni",
+        college: "Waseda University",
+        location: "Tokyo, Japan",
+        description: "Currently studying Business"
+    },
+    {
+        firstName: "Xavi",
+        lastName: "Pavon",
+        position: "Alumni",
+        college: "",
+        location: "Barcelona, Spain",
+        description: "Currently studying Business"
+    }
+];
+
+export default persons;
